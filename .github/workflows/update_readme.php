41b4437926d1dcb26e4091fceb0d2050a90345a2
polodu13160 <131604644+polodu13160@users.noreply.github.com>
2024-12-18 04:07:06 +0100

    Update update_readme.php

--- .github/workflows/update_readme.php
+++ .github/workflows/update_readme.php
@@ -8,22 +8,22 @@ $readmeTemplate = file_get_contents('README.md');
 
 function fetchGitHubData($username) {
     $client = new Client();
-    $oneWeekAgo = date('Y-m-d', strtotime('-1 week'));
-    $firstOfMonth = date('Y-m-d', strtotime('first day of this month'));
+   // $oneWeekAgo = date('Y-m-d', strtotime('-1 week'));
+    //$firstOfMonth = date('Y-m-d', strtotime('first day of this month'));
 
     // Echo the dates for debugging purposes
-    echo "Date one week ago: $oneWeekAgo\n";
-    echo "Date first of month: $firstOfMonth\n";
+   // echo "Date one week ago: $oneWeekAgo\n";
+   // echo "Date first of month: $firstOfMonth\n";
 
-    $commitsWeekResponse = $client->get("https://api.github.com/search/commits?q=author:$username+committer-date:>$oneWeekAgo", [
-        'headers' => ['Accept' => 'application/vnd.github.v3+json'],
-    ]);
-    $commitsWeek = json_decode($commitsWeekResponse->getBody(), true);
+    //$commitsWeekResponse = $client->get("https://api.github.com/search/commits?q=author:$username+committer-date:>$oneWeekAgo", [
+      //  'headers' => ['Accept' => 'application/vnd.github.v3+json'],
+    //]);
+    //$commitsWeek = json_decode($commitsWeekResponse->getBody(), true);
 
-    $commitsMonthResponse = $client->get("https://api.github.com/search/commits?q=author:$username+committer-date:>$firstOfMonth", [
-        'headers' => ['Accept' => 'application/vnd.github.v3+json'],
-    ]);
-    $commitsMonth = json_decode($commitsMonthResponse->getBody(), true);
+    //$commitsMonthResponse = $client->get("https://api.github.com/search/commits?q=author:$username+committer-date:>$firstOfMonth", [
+      //  'headers' => ['Accept' => 'application/vnd.github.v3+json'],
+    //]);
+    //$commitsMonth = json_decode($commitsMonthResponse->getBody(), true);
 
     $reposResponse = $client->get("https://api.github.com/users/$username/repos?sort=created&per_page=3");
     $repos = json_decode($reposResponse->getBody(), true);
@@ -35,14 +35,10 @@ function fetchGitHubData($username) {
 
     global $readmeTemplate;
     $updatedReadme = str_replace([
-        '{{ commits_week }}',
-        '{{ commits_month }}',
         '{{ recent_projects }}',
         '{{ github_username }}',
         '{{ last_update }}'
     ], [
-        $commitsWeek['total_count'],
-        $commitsMonth['total_count'],
         $recentProjects,
         $username,
         date('Y-m-d H:i:s')
