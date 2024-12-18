780b56c5219a3d9fb474771bcd801ff1a0e81bdd
polodu13160 <131604644+polodu13160@users.noreply.github.com>
2024-12-18 02:21:04 +0100

    Create update_readme.php

---
+++ .github/workflows/update_readme.php
@@ -0,0 +1,50 @@
+<?php
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+$username = 'polodu13160';
+$readmeTemplate = file_get_contents('README.md');
+
+function fetchGitHubData($username) {
+    $client = new Client();
+    $oneWeekAgo = date('c', strtotime('-1 week'));
+    $firstOfMonth = date('c', strtotime('first day of this month'));
+
+    $commitsWeekResponse = $client->get("https://api.github.com/search/commits?q=author:$username+committer-date:>$oneWeekAgo", [
+        'headers' => ['Accept' => 'application/vnd.github.v3+json'],
+    ]);
+    $commitsWeek = json_decode($commitsWeekResponse->getBody(), true);
+
+    $commitsMonthResponse = $client->get("https://api.github.com/search/commits?q=author:$username+committer-date:>$firstOfMonth", [
+        'headers' => ['Accept' => 'application/vnd.github.v3+json'],
+    ]);
+    $commitsMonth = json_decode($commitsMonthResponse->getBody(), true);
+
+    $reposResponse = $client->get("https://api.github.com/users/$username/repos?sort=created&per_page=3");
+    $repos = json_decode($reposResponse->getBody(), true);
+
+    $recentProjects = '';
+    foreach ($repos as $repo) {
+        $recentProjects .= "| 🟢 **{$repo['name']}** | [Accéder au repo]({$repo['html_url']}) |\n";
+    }
+
+    global $readmeTemplate;
+    $updatedReadme = str_replace([
+        '{{ commits_week }}',
+        '{{ commits_month }}',
+        '{{ recent_projects }}',
+        '{{ github_username }}',
+        '{{ last_update }}'
+    ], [
+        $commitsWeek['total_count'],
+        $commitsMonth['total_count'],
+        $recentProjects,
+        $username,
+        date('Y-m-d H:i:s')
+    ], $readmeTemplate);
+
+    file_put_contents('README.md', $updatedReadme);
+}
+
+fetchGitHubData($username);
